294923fe4787f7adaf69d290a646073c9dd12358
Nars <narsuluumamadinova@gmail.com>
2025-07-24 16:30:46 -0700

    Add user input handling for main menu options
    Introduces input prompt and conditional logic to handle user selection for creating an account, logging in, exiting, and invalid input in the FileBank System main menu.

--- bank-file-system/main.py
+++ bank-file-system/main.py
@@ -4,4 +4,21 @@ print("Welcome to FileBank System!")
 
 print("1. Create Account") 
 print("2. Log In")
-print("3. Exit")
+print("3. Exit")  
+
+user = input("Enter your choice: ") 
+
+if user == "1": 
+    print("Creating a new account...")
+
+elif user == "2":
+    print("Logging in...")
+
+elif user ==  "3":
+    print("Thank you for using FileBank. Goodbye! ") 
+
+    exit()
+
+else: 
+    print("Invalid input. Please choose 1, 2 or 3. ") 
+
